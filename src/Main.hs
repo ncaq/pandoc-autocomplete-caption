ccc959ae3498aa1089036eefedaffaf9a989931f
ncaq <ncaq@ncaq.net>
2015-05-10 00:43:17 +0900

    modified: div function

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,18 @@
 import           Data.Maybe
--- import           Text.Pandoc
--- import           Text.Pandoc.Walk
+import           Text.Pandoc
+import           Text.Pandoc.Walk
 
--- autocompleteCaption :: (ToJSON a) => a -> IO a
--- autocompleteCaption (Image [] src) = Image [Str $ fst src] src
--- autocompleteCaption b@(CodeBlock (i, c, v) s) | isNothing $ lookup "caption" v = CodeBlock (i, c, [("caption", i)]) s
---                                               | otherwise = b
--- autocompleteCaption x = x
+main :: IO ()
+main = interact (writeMarkdown def . walk autocompleteCaption . readMarkdown def)
 
--- main :: IO ()
--- main = interact (writeMarkdown def . walk autocompleteCaption . readMarkdown def)
+autocompleteCaption :: Block -> Block
+autocompleteCaption = bottomUp autocompleteInline . bottomUp autocompleteBlock
 
-main = interact id
+autocompleteInline :: Inline -> Inline
+autocompleteInline (Image [] src) = Image [Str $ fst src] src
+autocompleteInline x = x
+
+autocompleteBlock :: Block -> Block
+autocompleteBlock b@(CodeBlock (i, c, v) s) | isNothing $ lookup "caption" v = CodeBlock (i, c, [("caption", i)]) s
+                                            | otherwise = b
+autocompleteBlock x = x
